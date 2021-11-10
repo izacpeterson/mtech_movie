c4736ce80ec57128e730d4b55b0bce142176e4cc
izacpeterson <izacpeterson@gmail.com>
2021-11-10 12:36:29 -0700

    reverting some changes

--- public/app.js
+++ public/app.js
@@ -1,10 +1,6 @@
 const API_KEY = "26e99d56c670a23e5b53252a41402ce1";
 const URL = "https://api.themoviedb.org/3/";
 
-//API search endpoint
-const SEARCH = `search/movie/?api_key=${API_KEY}&query=`;
-const POPULAR = `movie/popular?api_key=${API_KEY}`;
-
 async function fetchData(url, callback) {
   let rawData = await fetch(url);
   let jsonData = await rawData.json();
--- public/search.js
+++ public/search.js
@@ -1,14 +1,5 @@
-// import { fetchData, URL, API_KEY } from "./app.js";
+import { fetchData, URL, API_KEY } from "./app.js";
 
-const API_KEY = "26e99d56c670a23e5b53252a41402ce1";
-const URL = "https://api.themoviedb.org/3/";
-
-async function fetchData(url, callback) {
-  let rawData = await fetch(url);
-  let jsonData = await rawData.json();
-
-  callback(jsonData);
-}
 //API search endpoint
 const SEARCH = `search/movie/?api_key=${API_KEY}&query=`;
 const POPULAR = `movie/popular?api_key=${API_KEY}`;
